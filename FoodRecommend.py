f279852b18a533117ebd89d70bdde8afae6ce48f
VesperLyn <107417537+VesperLyn@users.noreply.github.com>
2024-03-22 13:06:58 +0700

    New Function
    Added function to display food recommendation

--- FoodRecommend.py
+++ FoodRecommend.py
@@ -3,7 +3,7 @@ from sentence_transformers import SentenceTransformer
 import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
-
+from autocorrect import Speller
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
 if 'history' not in st.session_state:
@@ -17,16 +17,17 @@ user_profile.image(user_image, width=100)
 user_profile.write("Email: mathewjohnson@gmail.com")
 
 st.write("## Food Recommendation System!")
-st.write("Find your best food selection!")
-
+st.image("./homepage.jpg", use_column_width=True)
+st.write("Find your best food selection.")
 st.write("Search by ingredient, cuisine, or dietary restriction to discover your new favorite dish.")
 
 # input food ingredient or name
-food_input = st.text_input("What food are you in the mood for?", key='food_input')
+food_input = st.text_input("What food are you in the mood for?", key='food_input', placeholder="Enter food name / ingredient / description at least 3 character")
+speller = Speller(lang='en')
 
 # read data
 df = pd.read_csv('./Food Dataset.csv')
-df = df[['Customer', 'Food_Name', 'Food_Description']]
+df = df[['Food_Name', 'Food_Description']]
 
 def recommend_food(food_input):
     data = df[['Food_Name', 'Food_Description']]
@@ -39,14 +40,36 @@ def recommend_food(food_input):
 
     user_vectors = model.encode([food_input])
 
+    # finding similarities between vector
     similarities = cosine_similarity(user_vectors, vectors)
 
-    match_found = np.where(similarities >= 0.5)
+    # get 5 food with same similarity
+    # ascending sort
+    top_indices = np.argsort(similarities[0])[-5:][::-1]
+    recommendations = []
+    for i in top_indices:
+        food_name = data['Food_Name'][i]
+        food_description = data['Food_Description'][i]
+        recommendations.append((food_name, food_description))
+        if not recommendations:
+            return "Sorry, we cannot find the food to recommend."
+    return recommendations
 
-if food_input:
-    st.session_state.history.append(food_input)
+if st.button('Recommend Food'):
+    if food_input:
+        st.session_state.history.append(food_input)
 
-if st.session_state.history:
-    st.write("## History")
-    for i, value in enumerate(st.session_state.history):
-       st.write(f"{i+1}. {value}")
+    if st.session_state.history:
+        st.write("## History")
+        for i, value in enumerate(st.session_state.history):
+            st.write(f"{i+1}. {value}")
+
+    # display top 5 / can add up to 10
+    recommendations = recommend_food(food_input)
+    if recommendations:
+        st.write("## Top 5 Food Recommendations")
+        for i, (food_name, food_description) in enumerate(recommendations):
+            with st.expander(food_name):
+                st.write(f"{food_description}")
+    else:
+        st.write("No recommendations found.")
